bd564cd8d44047df54503aad39c600d2285f490d
Roman Gershgorin <roman.gershgorin@booking.com>
2023-07-24 09:45:18 +0200

    update layout

--- index.js
+++ index.js
@@ -1,55 +1,53 @@
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
-let map, infoWindow;
-
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+// This example displays a marker at the center of Australia.
+// When the user clicks the marker, an info window opens.
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 6,
-  });
-  infoWindow = new google.maps.InfoWindow();
-
-  const locationButton = document.createElement("button");
-
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
-        }
-      );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-  });
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
-  );
-  infoWindow.open(map);
-}
-
-window.initMap = initMap;
+    const uluru = { lat: -25.363, lng: 131.044 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    const contentString =
+      '<div id="content">' +
+      '<div id="siteNotice">' +
+      "</div>" +
+      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+      '<div id="bodyContent">' +
+      "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+      "sandstone rock formation in the southern part of the " +
+      "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+      "south west of the nearest large town, Alice Springs; 450&#160;km " +
+      "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+      "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+      "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+      "Aboriginal people of the area. It has many springs, waterholes, " +
+      "rock caves and ancient paintings. Uluru is listed as a World " +
+      "Heritage Site.</p>" +
+      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+      "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+      "(last visited June 22, 2009).</p>" +
+      "</div>" +
+      "</div>";
+    const infowindow = new google.maps.InfoWindow({
+      content: contentString,
+      ariaLabel: "Uluru",
+    });
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map,
+      title: "Uluru (Ayers Rock)",
+    });
+  
+    marker.addListener("click", () => {
+      infowindow.open({
+        anchor: marker,
+        map,
+      });
+    });
+  }
+  
+  window.initMap = initMap;
